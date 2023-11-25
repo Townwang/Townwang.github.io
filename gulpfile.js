f582b6e3e0b27584fa10f7d50394b09698aaae5b
Townwang <android@townwang.com>
2023-11-25 23:20:35 +0800

    Signed-off-by: Townwang <android@townwang.com>

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,26 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'), // 压缩css文件
     rename = require('gulp-rename'); // 文件重命名
 
-gulp.task('scripts', function(){
-    gulp.src('assets/js/index.js')
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('assets/js'))
-});
+//gulp.task('scripts', function(){
+//    gulp.src('assets/js/index.js')
+//        .pipe(uglify())
+//        .pipe(rename({suffix: '.min'}))
+//        .pipe(gulp.dest('assets/js'))
+//});
 
-gulp.task('sass', function(){
-    gulp.src('assets/sass/app.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/sass'))
-        .pipe(cleanCSS())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('assets/css'));
-});
+//gulp.task('sass', function(){
+//    gulp.src('assets/sass/app.scss')
+//        .pipe(sass())
+//        .pipe(gulp.dest('assets/sass'))
+//        .pipe(cleanCSS())
+//        .pipe(rename({suffix: '.min'}))
+//        .pipe(gulp.dest('assets/css'));
+//});
 
 gulp.task('watch', function(){
-    gulp.watch('assets/sass/*.scss', ['sass']);
-    gulp.watch('assets/js/*.js', ['scripts']);
+//    gulp.watch('assets/sass/*.scss', ['sass']);
+    gulp.watch('assets/css/*.css', ['css']);
+	gulp.watch('assets/js/*.js', ['scripts']);
 });
 
-gulp.task('default', ['scripts', 'sass', 'watch']);
+gulp.task('default', ['scripts', 'css', 'watch']);
